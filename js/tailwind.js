d6d5dc37593435b58410405974284f1c6691d573
songa1 <cishahayo.songa@gitstart.dev>
2022-09-29 10:24:05 +0200

    Set up tailwind and font

---
+++ js/tailwind.js
@@ -0,0 +1,15 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#395B64',
+                secondary: '#2C3333',
+                other: '#A5C9CA'
+            },
+            fontFamily: {
+                sans: ['Raleway', 'sans-serif']
+            },
+        }
+    }
+}
